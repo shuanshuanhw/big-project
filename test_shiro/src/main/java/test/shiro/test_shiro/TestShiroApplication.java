b7f4eec324ceeeb661bc9d4c1fae610eae75f58f
shuanshuan <178650140@qq.com>
2022-05-19 11:05:51 +0800

    20220519

---
+++ test_shiro/src/main/java/test/shiro/test_shiro/TestShiroApplication.java
@@ -0,0 +1,13 @@
+package test.shiro.test_shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestShiroApplication.class, args);
+    }
+
+}
---
+++ test_shiro/src/test/java/test/shiro/test_shiro/TestShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package test.shiro.test_shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
